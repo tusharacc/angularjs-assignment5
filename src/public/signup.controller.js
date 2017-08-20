edd1ae40ee9179d2a689576859496ee837f3af7d
Tushar Saurabh <tusharacc@gmail.com>
2017-08-20 20:07:39 +0000

    Working Assignment

--- src/public/signup.controller.js
+++ src/public/signup.controller.js
@@ -4,19 +4,45 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['SignUpService'];
-function SignUpController(SignUpService) {
+SignUpController.$inject = ['SignUpService','MenuAvailableService','$scope'];
+function SignUpController(SignUpService,MenuAvailableService,$scope) {
   var signUpCtrl = this;
-  
-//   var firstName = signUpCtrl.firstName;
-//   var lastName = signUpCtrl.lastName;
-//   var phoneNumber = signUpCtrl.phoneNumber;
-//   var email = signUpCtrl.email;
+  var shortName = [];
+  signUpCtrl.isPresent = false;
   
   signUpCtrl.saveDetails = function (user){
     console.log("First Name : ",user);
     
-    SignUpService.saveDetails(user.firstName,user.lastName,user.phoneNumber,user.email);  
+    var menuAvailable = MenuAvailableService.getAllMenuItems();
+    menuAvailable
+    .then(function(response){
+      response.data.menu_items.forEach(function(data){
+          shortName.push(data.short_name);
+      });
+      console.log('Short Name : ',shortName);
+      console.log('Menu Number : ',user.menunumber);
+      signUpCtrl.isPresent = shortName.find(function(element){
+          
+          if (element == user.menunumber){
+              return true;
+          }
+          
+      })? false: true;
+      
+      if (!signUpCtrl.isPresent){
+        SignUpService.saveDetails(user.firstName,user.lastName,user.phoneNumber,user.email,user.menunumber);
+        signUpCtrl.class = "alert alert-success";
+        signUpCtrl.message = "Thank You! Your details have been saved.";
+      } else {
+        signUpCtrl.class = "alert alert-danger";  
+        signUpCtrl.message = "Oh snap! Change a few things up and try submitting again.";
+      }
+      console.log('is Present : ',signUpCtrl.isPresent);
+    }).catch(function (errorResponse){
+        console.log(errorResponse.message);
+        signUpCtrl.alert = false;
+    });
+    
   };
 }
 
--- src/public/signup.service.js
+++ src/public/signup.service.js
@@ -9,12 +9,13 @@ SignUpService.$inject = [];
 function SignUpService() {
   var service = this;
   var details = [];
-  service.saveDetails = function (firstName,lastName,phoneNumber,email) {
+  service.saveDetails = function (firstName,lastName,phoneNumber,email,menunumber) {
     var detail = {};
     detail.firstName = firstName;
     detail.lastName = lastName;
     detail.phoneNumber = phoneNumber;
     detail.email = email;
+    detail.menunumber = menunumber;
     details.push(detail);
     console.log("Details Array while pushing", details);
     
